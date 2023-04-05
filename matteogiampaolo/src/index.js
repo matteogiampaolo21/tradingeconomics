e6b42784d2f2f7afcd0dd8b21d92dc3670127cde
Matteo Giampaolo <doyouthinkysauros@gmail.com>
2023-04-05 14:58:16 +0700

    see the latest news of countries.

--- matteogiampaolo/src/index.js
+++ matteogiampaolo/src/index.js
@@ -10,13 +10,17 @@ app.use(express.static(__dirname + '/public'));
 
 require("dotenv").config();
 
-// te.login(process.env.TRADE_KEY);
+te.login(process.env.TRADE_KEY);
 
 app.get("/", async (req, res) => {
-    // const data1 = await te.getNews(country = 'mexico', start_date = '2022-12-12', end_date = '2023-04-04');
-    // console.log(data1)
-    console.log(__dirname)
-    res.render("home");
+    const data1 = await te.getNews(start="0", limit="4");
+    res.render("news",{fourArticles : data1, titlePage :  "Latest" });
+});
+
+app.get("/news/:country", async (req, res) => {
+    const reqCountry = req.params.country
+    const data1 = await te.getNews(country = reqCountry);
+    res.render("news",{fourArticles : data1, titlePage :  data1[0].country});
 });
 
 
