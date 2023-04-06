7d4e35c44309551acf51cefdeb9400a0cf419992
Matteo Giampaolo <doyouthinkysauros@gmail.com>
2023-04-06 12:57:52 +0700

    will separated the latest and countries into two different pages.

--- matteogiampaolo/src/index.js
+++ matteogiampaolo/src/index.js
@@ -12,15 +12,28 @@ require("dotenv").config();
 
 te.login(process.env.TRADE_KEY);
 
-app.get("/", async (req, res) => {
-    const data1 = await te.getNews(start="0", limit="4");
-    res.render("news",{fourArticles : data1, titlePage :  "Latest" });
+app.get("/news/page/:number", async (req, res) => {
+    const numParam = req.params.number
+    const startNum = (parseInt(numParam)- 1)*8
+    const data1 = await te.getNews(start=`${startNum}`, limit="8");
+    let firstPageBoolean = false
+    if (numParam === "1"){
+        firstPageBoolean = true;
+    }
+    res.render("latestNews",{fourArticles : data1, titlePage :  "Latest" , firstPage : firstPageBoolean, pageNumber : parseInt(numParam)});
 });
 
 app.get("/news/:country", async (req, res) => {
     const reqCountry = req.params.country
     const data1 = await te.getNews(country = reqCountry);
-    res.render("news",{fourArticles : data1, titlePage :  data1[0].country});
+    console.log(data1.length)
+    res.render("latestNews",{fourArticles : data1, titlePage :  data1[0].country});
+});
+
+app.get("/test", async (req, res) => {
+    const data1 = await te.getNews(country = "mexico", start="10");
+    console.log(data1)
+    res.render("latestNews",{fourArticles : data1, titlePage :  data1[0].country});
 });
 
 
